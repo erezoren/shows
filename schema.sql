9cebd00c2e3862e82abdc75eee98ccdcca89eb56
eoren <eoren@L-TLV-13000834>
2017-09-17 20:47:05 +0300

    schema added

---
+++ schema.sql
@@ -0,0 +1,72 @@
+/*
+SQLyog Ultimate v10.0 
+MySQL - 5.7.9 : Database - showsaround
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`showsaround` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `showsaround`;
+
+/*Table structure for table `artists` */
+
+DROP TABLE IF EXISTS `artists`;
+
+CREATE TABLE `artists` (
+  `artist_id` int(200) unsigned NOT NULL AUTO_INCREMENT,
+  `artist_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `genre` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `main_image` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'http://www.bsmc.net.au/wp-content/uploads/No-image-available.jpg',
+  `comment` text COLLATE utf8_unicode_ci,
+  `last_modified_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`artist_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `artists` */
+
+/*Table structure for table `events` */
+
+DROP TABLE IF EXISTS `events`;
+
+CREATE TABLE `events` (
+  `event_id` int(200) unsigned NOT NULL AUTO_INCREMENT,
+  `artist_id` int(200) NOT NULL,
+  `venue_id` int(200) NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `time` datetime NOT NULL,
+  `comment` text COLLATE utf8_unicode_ci,
+  `last_modified_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`event_id`),
+  UNIQUE KEY `UNIQUE_EVENT` (`artist_id`,`venue_id`,`time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `events` */
+
+/*Table structure for table `venues` */
+
+DROP TABLE IF EXISTS `venues`;
+
+CREATE TABLE `venues` (
+  `venue_id` int(200) unsigned NOT NULL AUTO_INCREMENT,
+  `venue_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `price` int(11) NOT NULL DEFAULT '0',
+  `comment` text COLLATE utf8_unicode_ci,
+  `last_modified_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`venue_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `venues` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
